9f5ae2cdc1143836164399133939d3deb7db9e89
Gaurav Joshi <gj451026@gmail.com>
2022-09-14 16:27:07 +0530

    frontend is getting data from backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import App from './App';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
 const client = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Home.scss";
 import { useQuery } from "@tanstack/react-query";
+import { BiEditAlt } from "react-icons/bi";
+import { AiOutlineDelete } from "react-icons/ai";
 
-import {BiEditAlt} from 'react-icons/bi'
-import {AiOutlineDelete} from 'react-icons/ai'
 const fetcher = () => {
   return fetch(`http://127.0.0.1:8000/api/task-list/`).then((res) =>
     res.json()
@@ -11,11 +11,13 @@ const fetcher = () => {
 };
 
 function Home() {
-  const { data, isLoading } = useQuery(["todo-list"], () => fetcher());
+  // const [todo, setTodo] = useState('')
+
+  const { data: tasks, isLoading, isError } = useQuery(["todo-list"], () => fetcher());
 
   if (isLoading) return <h2>Loading....</h2>;
 
-  // const [todo, setTodo] = useState('')
+  if (isError) return <h2>Error with request</h2>;
 
   return (
     <div className="Home_container">
@@ -23,21 +25,17 @@ function Home() {
         <div className="Home_task-input">
           <input type="text" name="todo" placeholder="Add Todo..." />
         </div>
-        <div className="Home_task-post">
-          <input type="submit" />
-        </div>
+        <div className="Home_task-post"> <input type="submit" /> </div>
       </div>
       <div className="Home_List">
-        {data.map((task) => {
+        {tasks.map((task) => {
           console.log(task);
           return (
             <div className="Home_list-tasks" key={task.id}>
-              <div className="Home_list_tasks-title">
-                <p>{task.title}</p>
-              </div>
+              <div className="Home_list_tasks-title"> <p>{task.title}</p> </div>
               <div className="Home_list_tasks-button">
-                <p className="Home_list_tasks-edit"><BiEditAlt/></p>
-                <p className="Home_list_tasks-delete"> <AiOutlineDelete/> </p>
+                <p className="Home_list_tasks-edit"> <BiEditAlt /> </p>
+                <p className="Home_list_tasks-delete"> <AiOutlineDelete /> </p>
               </div>
             </div>
           );
