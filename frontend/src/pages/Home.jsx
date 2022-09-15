bd7a76984ef6e047a8fbd31c2548911bf28cd765
Gaurav Joshi <gj451026@gmail.com>
2022-09-15 12:16:54 +0530

    posting using usequery completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Home.scss";
-import { useQuery,useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import { BiEditAlt } from "react-icons/bi";
 import { AiOutlineDelete } from "react-icons/ai";
 import client from "../react-query-client";
 
-const fetcher = (url, body) => fetch(url, {
+const fetcher = (url, body) =>
+  fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -14,50 +15,73 @@ const fetcher = (url, body) => fetch(url, {
   });
 
 function Home() {
-  const [lang, setLang] = useState('')
+  const [lang, setLang] = useState("");
 
-  const mutation = useMutation((body) => fetcher('http://127.0.0.1:8000/api/task-create/', body), 
-  {
-    onSuccess(data) {
-      console.log("Got response from backend", data)
-      client.invalidateQueries('todos')
-      setLang(' ')
-    },
-    onError(error) {
-      console.log("Got error from backend", error);
-    },
-  })
+  const mutation = useMutation(
+    (body) => fetcher("http://127.0.0.1:8000/api/task-create/", body),
+    {
+      onSuccess(data) {
+        console.log("Got response from backend", data);
+        client.invalidateQueries("todos");
+        setLang(" ");
+      },
+      onError(error) {
+        console.log("Got error from backend", error);
+      },
+    }
+  );
 
-  const {data:tasks,isLoading,isError} = useQuery(['todos'], () => {
-    return fetch('http://127.0.0.1:8000/api/task-list/').then((t) => t.json())
-  })
+  const {
+    data: tasks,
+    isLoading,
+    isError,
+  } = useQuery(["todos"], () => {
+    return fetch("http://127.0.0.1:8000/api/task-list/").then((t) => t.json());
+  });
 
-  if(isLoading) return <h1>Loading...</h1>
+  if (isLoading) return <h1>Loading...</h1>;
 
-  if(isError) return <h1>Error with request</h1>
+  if (isError) return <h1>Error with request</h1>;
 
   function callMutation() {
-    mutation.mutate({todo :lang})
+    mutation.mutate({ title: lang });
   }
-
 
   return (
     <div className="Home_container">
       <div className="Home_Add">
         <div className="Home_task-input">
-          <input type="text" name="todo_title" value={lang} onChange={e => setLang(e.target.value)}  placeholder="Add Todo..."  />
+          <input
+            type="text"
+            name="todo_title"
+            value={lang}
+            onChange={(e) => setLang(e.target.value)}
+            placeholder="Add Todo..."
+          />
         </div>
-        <div className="Home_task-post"> <button onClick={callMutation}>Submit</button> </div>
+        <div className="Home_task-post">
+          {" "}
+          <button onClick={callMutation}>Submit</button>{" "}
+        </div>
       </div>
       <div className="Home_List">
         {tasks.map((task) => {
           console.log(task);
           return (
             <div className="Home_list-tasks" key={task.id}>
-              <div className="Home_list_tasks-title"> <p>{task.title}</p> </div>
+              <div className="Home_list_tasks-title">
+                {" "}
+                <p>{task.title}</p>{" "}
+              </div>
               <div className="Home_list_tasks-button">
-                <p className="Home_list_tasks-edit"> <BiEditAlt /> </p>
-                <p className="Home_list_tasks-delete"> <AiOutlineDelete /> </p>
+                <p className="Home_list_tasks-edit">
+                  {" "}
+                  <BiEditAlt />{" "}
+                </p>
+                <p className="Home_list_tasks-delete">
+                  {" "}
+                  <AiOutlineDelete />{" "}
+                </p>
               </div>
             </div>
           );
